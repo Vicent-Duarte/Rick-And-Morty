7bf5c111049e0148e777223ff3d67618b257ca9e
Vicent-Duarte <vicentduarte70@gmail.com>
2024-04-30 00:44:25 -0400

    rick and morty

---
+++ src/App.jsx
@@ -0,0 +1,75 @@
+import { useEffect, useRef, useState } from 'react';
+// import './App.css'
+import UseFetch from './Hooks/UseFetch';
+import LocationData from './components/LocationData';
+import ResidentCard from './components/ResidentCard';
+
+function App() {
+
+  const [inputValue, setInputValue] = useState(Math.floor(Math.random() * 126) + 1);
+
+  const [location, getLocation, isLoading, hasError] = UseFetch();
+
+
+  useEffect(() => {
+    const url = `hello-vicenthttps://rickandmortyapi.com/api/location/${inputValue}`;
+
+    getLocation(url);
+
+  }, [inputValue]);
+
+  const textInput = useRef();
+
+  const handleSubmit = (event) => {
+    event.preventDefault()
+    const newInputValue = textInput.current.value.toLowerCase().trim()
+
+    if (!isNaN(newInputValue) && parseInt(newInputValue) > 0) {
+      setInputValue(newInputValue)
+    } else {
+      console.log('Debes proporcionar un número válido y positivo')
+    }
+    textInput.current.value = ''
+  }
+
+  console.log(location);
+
+  return (
+    <>
+      {
+        isLoading ?
+          <h2>Loading...</h2>
+          :
+          <div className='app'>
+            <img className='app__fondo' src="./img-fondo.webp" alt="imagen" />
+            <form className='app__form' onSubmit={handleSubmit}>
+              <input className='app__input' type="text" ref={textInput} />
+              <button className='app__btn'>Search</button>
+            </form>
+            {
+              hasError || inputValue === '0' ?
+                <h2> Hey! you must provide an id from 1 to 126</h2>
+                :
+                <>
+                  <LocationData
+                    location={location}
+                  />
+                  <div className='app__container'>
+                    {
+                      location?.residents.map(resident => (
+                        <ResidentCard
+                          key={resident}
+                          url={resident}
+                        />
+                      ))
+                    }
+                  </div>
+                </>
+            }
+          </div>
+      }
+    </>
+  )
+}
+
+export default App
---
+++ src/components/ResidentCard.jsx
@@ -0,0 +1,34 @@
+import React, { useEffect } from 'react'
+import UseFetch from '../Hooks/UseFetch'
+// import './styles/ResidentCard.css'
+
+const ResidentCard = ({ url }) => {
+
+    const [resident, getResident] = UseFetch();
+
+    useEffect(() => {
+        getResident(url);
+    }, [])
+
+
+    return (
+        <article className='resident'>
+            <figure className='resident__img'>
+                <img src={resident?.image} alt="resident photo" />
+                <figcaption className='resident__status'>
+                    <div className={`resident__circle ${resident?.status}`}></div>
+                    <span>{resident?.status}</span>
+                </figcaption>
+            </figure>
+            <h3 className='resident__name'>{resident?.name}</h3>
+            <hr className='resident__line' />
+            <ul className='resident__list'>
+                <li className='resident__item'><span>Especie: </span><span>{resident?.species}</span></li>
+                <li className='resident__item'><span>Origin: </span><span>{resident?.origin.name}</span></li>
+                <li className='resident__item'><span>Eppisodes where appear: </span><span>{resident?.episode.length}</span></li>
+            </ul>
+        </article>
+    )
+}
+
+export default ResidentCard
